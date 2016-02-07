67548e871fef870af1419bade7cf953152668cce
Deli <tduroo@gmail.com>
2016-02-07 19:49:15 +0100

    version bump

--- src/main/java/com/deli/immersion/ImmersionMod.java
+++ src/main/java/com/deli/immersion/ImmersionMod.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class ImmersionMod
 {
     public static final String MOD_ID = "immersion";
-    public static final String VERSION = "0.1.1";
+    public static final String VERSION = "0.1.2";
 
     private FeatureManager _featureManager = new FeatureManager();
 
